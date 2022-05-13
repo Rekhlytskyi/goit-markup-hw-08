6e89f3ba00ad5749a6262921c598eba0e0cbfaa9
Rekhlytskyi <rekhlytskyi@outlook.com>
2022-05-14 00:14:26 +0300

    try

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,12 +1,14 @@
+(() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-menu-open]"),
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-    modal: document.querySelector("[data-menu]"),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-open");
+    refs.menu.classList.toggle('is-open');
   }
+})();
